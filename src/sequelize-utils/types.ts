388ba79b4041a8aa562dba3b359cea1395243bad
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-02-05 20:47:35 +0100

    feat(sequelize-utils): rename InferSequelizeModel util

--- src/sequelize-utils/types.ts
+++ src/sequelize-utils/types.ts
@@ -4,11 +4,11 @@ import { Model, InferAttributes, InferCreationAttributes } from 'sequelize';
  * @description Used to infer Model typings
  * @example
  * ```ts
- * export interface BookModel extends InferModel<BookModel> {
+ * export interface BookModel extends InferSequelizeModel<BookModel> {
  *  id: CreationOptional<number>;
  *  authorId: ForeignKey<number>;
  *  title: string;
  * }
  * ```
  */
-export type InferModel<M extends Model> = Model<InferAttributes<M>, InferCreationAttributes<M>>;
+export type InferSequelizeModel<M extends Model> = Model<InferAttributes<M>, InferCreationAttributes<M>>;
