7f5a95ead12b95d4617912d79829528a2241deeb
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-02-05 21:10:03 +0100

    refactor(schema): move model related schema utils

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,13 +1,3 @@
-export { default as exposeModel } from './exposeModel';
-export { default as scopedField } from './scopedField';
-export { default as genModelOffsetPagination } from './genModelOffsetPagination';
-export {
-  default as genModelOrderBy,
-  convertOrderByToSequelizeOrderItem,
-  OrderTypeEnum,
-  type OrderType,
-  type GenericOrderBy,
-} from './genModelOrderBy';
-export { default as genModelFilters } from './genModelFilters';
 export { default as cacheGraphQLType } from './cacheGraphQLType';
 export * from './context';
+export * from './model';
---
+++ src/schema/model/index.ts
@@ -0,0 +1,10 @@
+export { default as exposeModel } from './exposeModel';
+export { default as genModelOffsetPagination } from './genModelOffsetPagination';
+export {
+  default as genModelOrderBy,
+  convertOrderByToSequelizeOrderItem,
+  OrderTypeEnum,
+  type OrderType,
+  type GenericOrderBy,
+} from './genModelOrderBy';
+export { default as genModelFilters } from './genModelFilters';
