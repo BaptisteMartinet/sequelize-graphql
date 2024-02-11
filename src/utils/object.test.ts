d112e67ce354c227e4a65ad5696b4e15085c83e6
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-02-11 20:36:37 +0100

    feat(utils): implement mapRecord test suite

---
+++ src/utils/object.test.ts
@@ -0,0 +1,17 @@
+import { mapRecord } from './object';
+
+describe('mapRecord', () => {
+  const baseObj = {
+    a: 1,
+    b: 2,
+    c: 3,
+  } as const;
+
+  it('should increase record values by one', () => {
+    expect(mapRecord(baseObj, (val, key) => val + 1)).toMatchObject({ a: 2, b: 3, c: 4 });
+  });
+
+  it('should replace record values by their keys', () => {
+    expect(mapRecord(baseObj, (val, key) => key)).toMatchObject({ a: 'a', b: 'b', c: 'c' });
+  });
+});
