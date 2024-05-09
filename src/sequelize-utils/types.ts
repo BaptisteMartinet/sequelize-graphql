7a339e42baa3c3caf7cb2e7dae426c2b18ba8a94
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-05-09 18:47:43 +0200

    feat(sequelize-utils): implement IdType, DefaultAttributes and InferSequelizeModelWithDefaults

--- src/sequelize-utils/types.ts
+++ src/sequelize-utils/types.ts
@@ -1,4 +1,4 @@
-import type { Model, InferAttributes, InferCreationAttributes } from 'sequelize';
+import type { Model, InferAttributes, InferCreationAttributes, CreationOptional } from 'sequelize';
 
 /**
  * @description Used to infer Model typings
@@ -12,3 +12,15 @@ import type { Model, InferAttributes, InferCreationAttributes } from 'sequelize'
  * ```
  */
 export type InferSequelizeModel<M extends Model> = Model<InferAttributes<M>, InferCreationAttributes<M>>;
+
+/** The default sequelize-graphql id column type (ie. UUIDV4) */
+export type IdType = string;
+
+/** The default sequelize-graphql Model attributes */
+export interface DefaultAttributes {
+  id: CreationOptional<IdType>;
+  createdAt: CreationOptional<number>;
+  updatedAt: CreationOptional<number>;
+}
+
+export type InferSequelizeModelWithDefaults<M extends Model> = InferSequelizeModel<M & DefaultAttributes>;
