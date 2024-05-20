c7c57d7a754ad6fe9cb2445cfd81663d13fb7f29
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-05-20 02:17:50 +0200

    feat(utils/promise): implement optionalPromiseThen

--- src/utils/promise/optionalPromise.ts
+++ src/utils/promise/optionalPromise.ts
@@ -4,3 +4,11 @@ export function isPromise<T>(value: OptionalPromise<T>): value is Promise<T> {
   if (value && (value as Promise<T>).then) return true;
   return false;
 }
+
+export function optionalPromiseThen<ValueT, ResultT>(
+  value: OptionalPromise<ValueT>,
+  handler: (value: ValueT) => OptionalPromise<ResultT>,
+) {
+  if (isPromise(value)) return value.then(handler);
+  return handler(value);
+}
