91adabab8eebca461daa89b3eb8eed58222cd804
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-05-31 21:28:37 +0200

    feat(schema): add memoized utility to context

--- src/schema/context.ts
+++ src/schema/context.ts
@@ -1,9 +1,11 @@
 import { ModelLoader } from '@definitions/index';
+import { memoizer } from '@utils/memoize';
 
 export type Context = ReturnType<typeof makeContext>;
 
 export function makeContext() {
   return {
     modelLoader: new ModelLoader(),
+    memoized: memoizer(),
   } as const;
 }
