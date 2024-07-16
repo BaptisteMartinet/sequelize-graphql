bea3105236142c30efd58226a84f0fa900d9c6d6
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-07-16 19:50:44 +0200

    feat(definitions/model): disable foreign keys constraints

--- src/definitions/model/Model.ts
+++ src/definitions/model/Model.ts
@@ -1,4 +1,10 @@
-import type { Model as SequelizeModel, Identifier, IncludeOptions, CountOptions, Attributes } from 'sequelize';
+import type {
+  Model as SequelizeModel,
+  Identifier,
+  IncludeOptions,
+  CountOptions,
+  Attributes,
+} from 'sequelize';
 import type { Context } from '@schema/index.js';
 import type { ModelDefinition, AssociationDefinition, AssociationSpecs } from './types.js';
 
@@ -40,14 +46,21 @@ export default class Model<M extends SequelizeModel> {
           as: associationName,
           foreignKey,
           onDelete,
+          constraints: false,
         });
       case 'hasOne':
-        return this._model.hasOne(targetModel.model, { as: associationName, foreignKey, onDelete });
+        return this._model.hasOne(targetModel.model, {
+          as: associationName,
+          foreignKey,
+          onDelete,
+          constraints: false,
+        });
       case 'hasMany':
         return this._model.hasMany(targetModel.model, {
           as: associationName,
           foreignKey,
           onDelete,
+          constraints: false,
         });
       default:
         throw new Error(`Invalid association type: ${type}`);
@@ -124,9 +137,7 @@ export default class Model<M extends SequelizeModel> {
   public async ensureExistence(identifier: Identifier, opts: { ctx?: Context } = {}) {
     const instance = await this.findByPkAllAttrs(identifier, opts);
     if (instance === null)
-      throw new Error(
-        `EnsureExistence check failed for model ${formatModelId(this, identifier)}`,
-      );
+      throw new Error(`EnsureExistence check failed for model ${formatModelId(this, identifier)}`);
     return instance;
   }
 
