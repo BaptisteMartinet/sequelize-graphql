24f6d815966a3088ddd343a457b71f5cf27364ed
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-08-01 20:11:53 +0200

    refactor: improve export paths

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './definitions/index';
-export * from './schema/index';
+export * from './definitions';
+export * from './schema';
 export * from './graphql-utils';
 export * from './sequelize-utils';
