c1532d325287361ea7802d8276e17cbf5e92d2f6
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-08-04 17:52:51 +0200

    refactor(graphql-utils): simplify GraphQLDate scalar

--- src/graphql-utils/scalars/GraphQLDate.ts
+++ src/graphql-utils/scalars/GraphQLDate.ts
@@ -3,24 +3,19 @@ import { GraphQLScalarType, Kind } from 'graphql';
 const GraphQLDate = new GraphQLScalarType({
   name: 'Date',
   description: 'Date custom scalar type',
+  // Convert outgoing
   serialize(value) {
-    if (value instanceof Date) {
-      return value.getTime(); // Convert outgoing Date to integer for JSON
-    }
+    if (value instanceof Date) return value.getTime();
     throw Error('GraphQL Date Scalar serializer expected a `Date` object');
   },
+  // Convert incoming
   parseValue(value) {
-    if (typeof value === 'number') {
-      return new Date(value); // Convert incoming integer to Date
-    }
+    if (typeof value === 'number') return new Date(value);
     throw new Error('GraphQL Date Scalar parser expected a `number`');
   },
+  // Convert hard-coded AST
   parseLiteral(ast) {
-    if (ast.kind === Kind.INT) {
-      // Convert hard-coded AST string to integer and then to Date
-      return new Date(parseInt(ast.value, 10));
-    }
-    // Invalid hard-coded value (not an integer)
+    if (ast.kind === Kind.INT) return new Date(parseInt(ast.value, 10));
     return null;
   },
 });
