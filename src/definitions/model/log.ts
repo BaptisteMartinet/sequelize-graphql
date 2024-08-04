b84906982ddc87ac1299a85e328b92e46216e4f9
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-08-04 19:14:23 +0200

    feat(definitions/models): change formatModelId separator

--- src/definitions/model/log.ts
+++ src/definitions/model/log.ts
@@ -3,5 +3,5 @@ import type Model from './Model';
 
 /** @returns A string formatted like so "[ModelName]#[identifier]" (e.g. "User#1234") */
 export function formatModelId(model: Model<any>, identifier: Identifier) {
-  return `${model.name}$${identifier}`;
+  return `${model.name}#${identifier}`;
 }
