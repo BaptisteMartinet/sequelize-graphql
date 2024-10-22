f9a8aa7cbdfccec240876438f57ea297f057958c
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-10-22 23:03:04 +0200

    feat(model-schema): handle timestamps in genModelFilters

--- src/schema/model/genModelFilters.ts
+++ src/schema/model/genModelFilters.ts
@@ -4,11 +4,12 @@ import type { Model } from '@definitions/index';
 import assert from 'assert';
 import { GraphQLInputObjectType } from 'graphql';
 import { filterRecord, mapRecord } from '@utils/object';
+import { DateFilter } from '@definitions/model/columnTypesFilters';
 import { cacheGraphQLType } from '@schema/index';
 
 export default function genModelFilters<M extends SequelizeModel>(model: Model<M>) {
   const { name, definition } = model;
-  const { columns } = definition;
+  const { columns, timestamps } = definition;
   const filterableColumns = filterRecord(columns, (column) => {
     return column.type.filterGqlType !== undefined && (column.filterable ?? column.exposed);
   });
@@ -21,6 +22,12 @@ export default function genModelFilters<M extends SequelizeModel>(model: Model<M
           assert(column); // Used for type safety. Should never trigger.
           return { type: column.type.filterGqlType };
         }),
+        ...(timestamps === undefined || timestamps === true
+          ? {
+              createdAt: { type: DateFilter },
+              updatedAt: { type: DateFilter },
+            }
+          : null),
       },
     }),
   );
